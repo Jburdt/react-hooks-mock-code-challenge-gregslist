34e3dcf1e99434488ba878da700378546cb59aa7
Josh Burd <Jburdt@gmail.com>
2022-09-27 15:54:20 -0400

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+
+const [listings, setListings] = useState([])
+const [searchTerm, setSearchTerm] = useState("")
+
+const filteredTerms = () => {
+  return listings.filter((listObj) => ( listObj.description.toLowerCase().includes(searchTerm.toLowerCase()) || searchTerm === "")) 
+}
+
+
+// console.log(searchTerm)
+// console.log(filteredTerms())
+//grabs form api to put on page
+useEffect(() => {
+  fetch('http://localhost:6001/listings')
+  .then((res) => res.json())
+  .then((data) => setListings(data))
+}, [])
+
+//deletes form listngs
+const handleDelete = (id) => {
+
+  const newListing = listings.filter((listObj) => listObj.id !== id) 
+  setListings(newListing) 
+};
+// console.log(listings)
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <ListingsContainer handleDelete={handleDelete} listings={filteredTerms()} />
     </div>
   );
 }
